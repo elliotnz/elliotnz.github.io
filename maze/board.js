8b717fde12790b592e6101e2d88a56392464a2f8
Elliot <elliot.baguley@gmail.com>
2014-11-02 17:59:16 +1300

    elliots monster

--- maze/board.js
+++ maze/board.js
@@ -31,18 +31,93 @@ function Man(board, x, y, pixelWidth, context, colour) {
   this.turn = function() {}
 }
 
-function Monster(board, x, y, pixelWidth, context, colour)  {
+function Monster(board, man, x, y, pixelWidth, context, colour)  {
   this.x = x;
   this.y = y;
   this.pixelWidth = pixelWidth;
   this.context = context;
   this.colour = colour;
   this.board = board;
+  this.man = man;
 
   this.turn = function() {
-    this.x -= 1;
-    if (this.x == 0)
-      this.x = board.width - 1;
+    var direction = 180;
+    counter = 0;
+    var moved = false;
+    if ((this.x > this.man.x)) {
+      if (this.board.canMoveTo(this.x - 1, this.y)) {
+        direction = 270;
+        counter += 1;
+        moved = true;
+        this.x -= 1;
+      }
+    }
+    if ((this.x < this.man.x)) {
+      if (this.board.canMoveTo(this.x + 1, this.y)) {
+        counter += 1;
+        direction = 90;
+        moved = true;
+        this.x += 1;
+      }
+    }
+    if ((this.y < this.man.y)) {
+      if (this.board.canMoveTo(this.x, this.y + 1)) {
+        counter += 1;
+        direction = 180;
+        moved = true;
+        this.y += 1;
+      }
+    }
+    if ((this.y > this.man.y)) {
+      if (this.board.canMoveTo(this.x, this.y - 1)) {
+        counter += 1;
+        direction = 0;
+        moved = true;
+        this.y -= 1;
+      }
+    }
+    if (this.x == man.x && this.y == man.y) {
+      board.captured();
+    }
+
+    if (!moved) {
+       loc = this.board.spawnLocation()
+      if (this.x < 0) {
+        counter += 1;
+        this.x = loc.x;
+      } else if (this.x > this.board.width) {
+        counter += 1;
+        this.x = loc.x;
+      } else if (this.y < 0) {
+        counter += 1;
+        this.y = loc.y;
+      } else if (this.y > this.board.height) {
+        counter += 1;
+        this.y = loc.y;
+      } else {
+        if (direction === 270 && canMoveTo(this.x + 1, this.y)) {
+          counter += 1;
+          this.x -= 1
+        }
+        if (direction === 90 && canMoveTo(this.x - 1, this.y)) {
+          counter += 1;
+          this.x -= 1
+        }
+        if (direction === 180 && canMoveTo(this.x, this.y - 1)) {
+          counter += 1;
+          this.y -= 1
+        }
+        if (direction === 0 && canMoveTo(this.x, this.y + 1)) {
+          counter += 1;
+          this.y += 1
+        }
+
+      }
+    }
+    if (counter > 3) {
+      counter = 0;
+      direction += 90 % 360;
+    }
   }
 }
 
@@ -121,7 +196,7 @@ function Board(width, height, pixelWidth, context) {
   }
 
   this.canMoveTo = function(x, y) {
-    return ((this.cells[x][y] === 0) && (x >= 0) && (x < this.width) && (y >= 0) && (y < this.height));
+    return ((x >= 0) && (x < this.width) && (y >= 0) && (y < this.height) && (this.cells[x][y] === 0));
   }
 
   this.drawGrid = function() {
@@ -158,6 +233,19 @@ function Board(width, height, pixelWidth, context) {
       this.things[i].turn()
     }
   }
+
+  this.spawnLocation = function() {
+    while (true) {
+      x = Math.floor(Math.random() * this.width);
+      y = Math.floor(Math.random() * this.height);
+      if (this.canMoveTo(x,y))
+        return {x: x, y: y}
+    }
+  }
+
+  this.captured = function() {
+    alert('boo')
+  }
 }
 
 var start = function() {
@@ -168,8 +256,10 @@ var start = function() {
   canvas.style.width = '' + (board.width * 10)  + 'px';
   canvas.style.height = '' + (board.height * 10) + 'px' ;
 
-  var man = new Man(board, 15, 15, 10, context, "red");
-  var monster = new Monster(board, 12, 35, 10, context, "blue");
+  var loc = board.spawnLocation()
+  var man = new Man(board, loc.x, loc.y, 10, context, "red");
+  loc = board.spawnLocation()
+  var monster = new Monster(board, man, loc.x , loc.y, 10, context, "blue");
   board.add(man);
   board.add(monster);
 
