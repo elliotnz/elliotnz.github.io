b706d3084e0407036bf58988e8b42f3c4a28d922
Elliot <elliot.baguley@gmail.com>
2014-11-04 19:30:28 +1300

    Monsters Working Well

--- maze/board.js
+++ maze/board.js
@@ -1,10 +1,4 @@
-function Man(board, x, y, pixelWidth, context, colour) {
-  this.x = x;
-  this.y = y;
-  this.pixelWidth = pixelWidth;
-  this.context = context;
-  this.colour = colour;
-  this.board = board;
+function Thing(board, x, y, pixelWidth, context, colour) {
 
   this.draw = function() {
     this.context.fillStyle=this.colour;
@@ -12,24 +6,43 @@ function Man(board, x, y, pixelWidth, context, colour) {
   }
 
   this.moveup = function() {
-    if (this.board.canMoveTo(this.x, this.y - 1))
+    if (this.board.canMoveTo(this, this.x, this.y - 1))
       this.y -= 1;
   }
   this.movedown = function() {
-    if (this.board.canMoveTo(this.x, this.y + 1))
+    if (this.board.canMoveTo(this, this.x, this.y + 1))
       this.y += 1;
   }
   this.moveleft = function() {
-    if (this.board.canMoveTo(this.x - 1, this.y))
+    if (this.board.canMoveTo(this, this.x - 1, this.y))
       this.x -= 1;
   }
   this.moveright = function() {
-    if (this.board.canMoveTo(this.x + 1, this.y))
+    if (this.board.canMoveTo(this, this.x + 1, this.y))
       this.x += 1;
   }
 
+  this.isMonster = function() {
+    return false;
+  }
+
   this.turn = function() {}
+
+  this.checkAlive = function() {}
 }
+
+function Man(board, x, y, pixelWidth, context, colour) {
+  this.x = x;
+  this.y = y;
+  this.pixelWidth = pixelWidth;
+  this.context = context;
+  this.colour = colour;
+  this.board = board;
+}
+
+Man.prototype = new Thing();
+
+
 
 function Monster(board, man, x, y, pixelWidth, context, colour)  {
   this.x = x;
@@ -44,45 +57,105 @@ function Monster(board, man, x, y, pixelWidth, context, colour)  {
   this.change_x = 0;
   this.change_y = 0;
 
+  this.isMonster = function() {
+    return true;
+  }
+
   this.turn = function() {
-    var moved = false;
-    if (this.counter % 4 == 0) {
-      this.change_x = 0; this.change_y = 0;
-      if ((this.x > this.man.x)) {
-          this.change_x = -1;
+    if (this.board.attack === false) {
+      var moved = false;
+      if (this.counter % 4 == 0) {
+        this.change_x = 0; this.change_y = 0;
+        if ((this.x > this.man.x)) {
+            this.change_x = -1;
+        }
+        if ((this.x < this.man.x)) {
+            this.change_x = 1;
+        }
+        if ((this.y < this.man.y)) {
+            this.change_y = 1;
+        }
+        if ((this.y > this.man.y)) {
+            this.change_y = -1;
+        }
       }
-      if ((this.x < this.man.x)) {
-          this.change_x = 1;
-      }
-      if ((this.y < this.man.y)) {
-          this.change_y = 1;
-      }
-      if ((this.y > this.man.y)) {
-          this.change_y = -1;
-      }
-    }
 
-    if (this.board.canMoveTo(this.x + this.change_x, this.y + this.change_y)) {
-      this.x += this.change_x;
-      this.y += this.change_y;
-      moved = true;
-      //this.counter = 0;
-    }
+      if (this.board.canMoveTo(this, this.x + this.change_x, this.y + this.change_y)) {
+        this.x += this.change_x;
+        this.y += this.change_y;
+        moved = true;
+        //this.counter = 0;
+      }
 
-    if (!moved) {
-      // lets try a random direction
-      this.change_y = Math.floor(Math.random() * 3) - 1;
-      this.change_x = Math.floor(Math.random() * 3) - 1;
-    }
+      if (!moved) {
+        // lets try a random direction
+        this.change_y = Math.floor(Math.random() * 3) - 1;
+        this.change_x = Math.floor(Math.random() * 3) - 1;
+      }
+      if (this.x == man.x && this.y == man.y) {
+        board.captured();
+      }
+      this.counter += 1;
+    } else if (this.board.attack === true) {
+      var moved = false;
+      if (this.counter % 4 == 0) {
+        this.change_x = 0; this.change_y = 0;
+        if ((this.x < this.man.x)) {
+            this.change_x = -1;
+        }
+        if ((this.x > this.man.x)) {
+            this.change_x = 1;
+        }
+        if ((this.y > this.man.y)) {
+            this.change_y = 1;
+        }
+        if ((this.y < this.man.y)) {
+            this.change_y = -1;
+        }
+      }
 
-    if (this.x == man.x && this.y == man.y) {
-      board.captured();
+      if (this.board.canMoveTo(this, this.x + this.change_x, this.y + this.change_y)) {
+        this.x += this.change_x;
+        this.y += this.change_y;
+        moved = true;
+        //this.counter = 0;
+      }
+
+      if (!moved) {
+        // lets try a random direction
+        this.change_y = Math.floor(Math.random() * 3) - 1;
+        this.change_x = Math.floor(Math.random() * 3) - 1;
+      }
+      if (this.x === man.x && this.y === man.y) {
+        var newLocation = this.board.spawnLocation()
+        this.x = newLocation.x;
+        this.y = newLocation.y;
+      }
+      this.counter += 1;
     }
-    this.counter += 1;
   }
 }
 
-Monster.prototype = new Man();
+function Energy(board, man, x, y, pixelWidth, context)  {
+  this.x = x;
+  this.y = y;
+  this.pixelWidth = pixelWidth;
+  this.board = board;
+  this.man = man;
+  this.context = context;
+  this.colour="green";
+
+  this.checkAlive = function() {
+    if (this.board.attack == false && man.x == this.x && man.y == this.y) {
+      alert("Attack")
+      this.board.attack = true;
+    }
+  }
+
+}
+
+Monster.prototype = new Thing();
+Energy.prototype = new Thing();
 
 function Board(width, height, pixelWidth, context) {
   this.width = width;
@@ -93,6 +166,9 @@ function Board(width, height, pixelWidth, context) {
   this.context.canvas.width = (width + 1) * pixelWidth;
   this.context.canvas.height = (height + 1) * pixelWidth;
   this.deaths = 0;
+  this.lives = 5;
+  this.health = 100;
+  this.attack = false
 
   this.createCellArray = function() {
     var cells = new Array(this.width);
@@ -110,46 +186,11 @@ function Board(width, height, pixelWidth, context) {
   this.cells = this.createCellArray();
 
   this.createMaze = function() {
-    x = this.width; y = this.height;
-  	var n=x*y-1;
-  	if (n<0) {alert("illegal maze dimensions");return;}
-  	var horiz =[]; for (var j= 0; j<x+1; j++) horiz[j]= [],
-  	    verti =[]; for (var j= 0; j<y+1; j++) verti[j]= [],
-  	    here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)],
-  	    path = [here],
-  	    unvisited = [];
-  	for (var j = 0; j<x+2; j++) {
-  		unvisited[j] = [];
-  		for (var k= 0; k<y+1; k++)
-  			unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
-  	}
-  	while (0<n) {
-  		var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-  		    [here[0]-1, here[1]], [here[0],here[1]-1]];
-  		var neighbors = [];
-  		for (var j = 0; j < 4; j++)
-  			if (unvisited[potential[j][0]+1][potential[j][1]+1])
-  				neighbors.push(potential[j]);
-  		if (neighbors.length) {
-  			n = n-1;
-  			next= neighbors[Math.floor(Math.random()*neighbors.length)];
-  			unvisited[next[0]+1][next[1]+1]= false;
-  			if (next[0] == here[0])
-  				horiz[next[0]][(next[1]+here[1]-1)/2]= true;
-  			else
-  				verti[(next[0]+here[0]-1)/2][next[1]]= true;
-  			path.push(here = next);
-  		} else
-  			here = path.pop();
-  	}
-
     for (var i=0; i < this.width; i += 1) {
       for (var j=0; j < this.height; j += 1) {
-        this.cells[i][j] = (horiz[i][j] && verti[i][j]) ? 1 : 0
+        this.cells[i][j] = (Math.random() > 0.23) ? 0 : 1
       }
     }
-
-  	return {x: x, y: y, horiz: horiz, verti: verti};
   }
 
 
@@ -157,8 +198,23 @@ function Board(width, height, pixelWidth, context) {
     this.things.push(object);
   }
 
-  this.canMoveTo = function(x, y) {
-    return ((x >= 0) && (x < this.width - 1) && (y >= 0) && (y < this.height - 1) && (this.cells[x][y] === 0));
+  this.canMoveTo = function(thing, x, y) {
+    var withinBoardAndNotWall = ((x >= 0) && (x < this.width)
+      && (y >= 0) && (y < this.height)
+      && (this.cells[x][y] === 0));
+    if (!withinBoardAndNotWall)
+      return false;
+    if (thing != null && thing.isMonster()) {
+      for (var i = 0; i< this.things.length; i++) {
+        if (this.things[i].isMonster() && thing !== this.things[i]) {
+          var isMonsterIntoMonster = (x === this.things[i].x && y === this.things[i].y)
+          if (isMonsterIntoMonster) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
   this.drawGrid = function() {
@@ -192,8 +248,14 @@ function Board(width, height, pixelWidth, context) {
   }
 
   this.turn = function() {
-    for (i = 0; i< this.things.length; i++) {
+    for (var i = 0; i< this.things.length; i++) {
       this.things[i].turn()
+    }
+  }
+
+  this.checkAlive = function() {
+    for (var i = 0; i< this.things.length; i++) {
+      this.things[i].checkAlive()
     }
   }
 
@@ -201,14 +263,26 @@ function Board(width, height, pixelWidth, context) {
     while (true) {
       x = Math.floor(Math.random() * this.width);
       y = Math.floor(Math.random() * this.height);
-      if (this.canMoveTo(x,y))
+      if (this.canMoveTo(null, x,y))
         return {x: x, y: y}
     }
   }
 
   this.captured = function() {
-    this.deaths += 1
-    document.getElementById('deaths').innerHTML = "" + this.deaths;
+    if (this.attack === false) {
+      if (this.lives <= 0) {
+        this.lives = 5;
+        this.deaths += 1;
+        document.getElementById('deaths').innerHTML = "DEATHS: " + this.deaths;
+      } else if (this.health <= 0) {
+        this.health = 120;
+        this.lives -= 1;
+        document.getElementById('lives').innerHTML = "LIVES: " + this.lives;
+      } else if (this.health > 0){
+        this.health -= 20;
+        document.getElementById('health').innerHTML = "HEALTH: " + this.health;
+      }
+    }
   }
 }
 
@@ -221,13 +295,16 @@ var start = function() {
   canvas.style.height = '' + (board.height * 10) + 'px' ;
 
   var loc = board.spawnLocation()
-  var man = new Man(board, loc.x, loc.y, 10, context, "red");
+  var location = board.spawnLocation()
+  var man = new Man(board, loc.x, loc.y, 10, context, "blue");
   loc = board.spawnLocation()
-  var monster = new Monster(board, man, loc.x , loc.y, 10, context, "blue");
-  var monster2 = new Monster(board, man, loc.x , loc.y, 10, context, "yellow");
+  var monster = new Monster(board, man, loc.x, loc.y + 10, 10, context, "yellow");
+  loc = board.spawnLocation()
+  var monster2 = new Monster(board, man, loc.x + 10, loc.y, 10, context, "red");
   board.add(man);
   board.add(monster);
   board.add(monster2);
+
 
   document.onkeydown = function(e) {
     e = e || window.event;
@@ -241,12 +318,42 @@ var start = function() {
       man.moveright()
     }
   }
-
+  var points = 0;
 
   setInterval(function() {
     board.drawCells();
   }, 1000 / 60);
   setInterval(function() {
+    if (board.deaths < 1) {
+      var pointsInterval = setInterval(function() {
+        points += 1
+        document.getElementById("points").innerHTML = "POINTS: " + points;
+        clearInterval(pointsInterval)
+      }, 100)
+    }
+  }, 1000)
+  setInterval(function() {
+    loc = board.spawnLocation()
+    var energy = new Energy(board, man, loc.x, loc.y, 10, context);
+    board.add(energy);
+    energy.turn()
+  }, 10000)
+  setInterval(function() {
+    if (Monster.x == man.x && Monster.y == man.y) {
+      if (board.attack === false) {
+        board.captured();
+      } else if (board.attack === true) {
+        loc = board.spawnLocation()
+        Monster.x = loc.x;
+        Monster.y = loc.y;
+      }
+    }
+  }, 1000 / 60)
+  setInterval(function() {
     board.turn();
   }, 1000 / 5);
+
+  setInterval(function() {
+    board.checkAlive();
+  }, 10);
 }
