390009116b23d5a72eb4d9264264f33544d0c7d0
Elliot <elliot.baguley@gmail.com>
2014-11-15 13:33:10 +1300

    fixed up turn

--- jump/script.js
+++ jump/script.js
@@ -8,106 +8,64 @@ function Thing(board, x, y) {
   this.turn = function() {}
 
 }
-function Man(board, line, x, y) {
+function Man(board, x, y, height, width) {
   this.x = x;
   this.y = y;
   this.colour = "blue";
   this.board = board;
-  this.line = line;
   this.lineAbove = false;
   this.lineBelow = false;
-  this.height = 10;
-  this.width = 5;
+  this.height = height;
+  this.width = width;
+  this.step = 1;
 
   this.draw = function() {
     this.board.context.fillStyle = "blue";
-    this.board.context.fillRect(this.x, this.y - this.height + 1, this.width, this.height)
+    this.board.context.fillRect(this.x, this.y, this.width, this.height)
   }
 
   this.turn = function() {
-    if (this.vForce == null) {
-
-    } else if (this.vForce >= 0) {
-      if (this.lineBelow) {
-        this.lineBelow = false;
-      }
-      if ((this.y >= this.line.y && this.y - 2.5 < this.line.y + this.height + this.line.height) && (this.x <= this.line.x + this.line.width && this.x >= this.line.x)) {
-        this.vForce -= 1;
-        this.lineAbove = true;
-      } else if (!this.lineAbove){
-        this.vForce -= 1;
-        this.y -= (this.vForce * .7);
-      }
-    } else if (this.vForce < 0) {
-      if (this.lineBelow) {
-        this.lineBelow = false;
-      }
-      if ((this.y <= this.line.y && this.y + 2.5 > this.line.y) && (this.x <= this.line.x + this.line.width && this.x >= this.line.x)) {
-        this.lineBelow = true;
-      } else if (!this.lineBelow) {
-        this.vForce -= .25;
-        this.y -= (this.vForce * .7);
-      }
-      if (this.x < this.line.x + this.line.width && this.x > this.line.x && this.y < this.line.y && this.y > this.line.y + this.line.height) {
-        //this.lineBelow = false;
-        this.lineBelow = true;
-      }
-      // if ((this.x > this.line.x) && (parseInt(this.y) + 1 === this.line.y)) {
-      //   this.lineBelow = false;
-      // }
-      if ((this.board.onGround(this))) {
-        this.lineBelow = false;
-        this.vForce = null;
-      }
-      if (this.lineBelow) {
-        this.lineBelow = true;
-        //this.vForce = null;
-      } else {
-        this.lineBelow = false;
-      }
+    // look above us
+    if (this.vForce > 0 && (!this.board.isBlocked(this.x, this.y - 1) && !this.board.isBlocked(this.x + this.width, this.y - 1))) {
+      this.y -= 1; //parseInt(this.vForce * .7);
+      this.vForce -= 1;
+    } else if (!this.board.isBlocked(this.x, this.y + this.height + 1) && !this.board.isBlocked(this.x + this.width, this.y + this.height + 1)) {
+      // if we are falling
+      this.y += 1;
+      this.vForce = null;
+    } else {
+      this.vForce = null;
     }
-    if (this.board.canMoveRight(this.x - 1, this.y)) {
-      if (this.board.canMoveLeft(this.x - 1, this.y)) {
-        this.x += this.xChange * 3;
-        this.xChange = 0;
-        this.lineAbove = false;
-        //this.lineBelow = false;
-      }
+    if ((this.xChange < 0 && !this.board.isBlocked(this.x - 1, this.y)) ||
+       (this.xChange > 0 && !this.board.isBlocked(this.x + this.width + 1, this.y))) {
+       this.x += this.xChange * this.step;
     }
+    this.xChange = 0;
   }
   this.jump = function() {
-    if ((this.board.onGround(this)) || this.lineBelow) {
-      this.vForce = 10;
-    }
+    if (this.vForce == null)
+      this.vForce = 30;
   }
+
   this.moveleft = function() {
-    if (this.board.canMoveLeft(this.x - 1 * 3, this.y)) {
       this.xChange = -1;
-    }
   }
   this.moveright = function() {
-    if (this.board.canMoveRight(this.x + 1 * 10, this.y)) {
      this.xChange = 1;
-    }
   }
 }
 Man.prototype = new Thing()
 
-function Line(board, x, y) {
+function Line(board, x, y, width, height) {
   this.board = board
   this.x = x;
   this.y = y;
-  this.width = 25;
-  this.height = 5;
+  this.width = width;
+  this.height = height;
 
   this.draw = function() {
     this.board.context.fillStyle = "black";
-    // this.board.context.fillRect(this.x, this.y, this.width, this.y)
     this.board.context.fillRect(this.x, this.y, this.width, this.height)
-    // this.board.context.beginPath();
-    // this.board.context.moveTo(this.x, this.y);
-    // this.board.context.lineTo(this.width, this.y);
-    // this.board.context.stroke();
   }
 }
 
@@ -125,7 +83,7 @@ function Board(width, height, pixelWidth, context) {
   this.context.canvas.style.height = '' + (height * pixelWidth) + 'px';
   this.keyMap = [];
   this.man = null;
-  this.line = null;
+  this.lines = [];
 
   this.add = function(thing) {
     this.things.push(thing);
@@ -133,8 +91,9 @@ function Board(width, height, pixelWidth, context) {
   this.addMan = function(man) {
     this.man = man;
   }
-  this.addLine = function(line) {
-    this.line = line;
+  this.addLines = function(line) {
+    this.lines.push(line);
+    this.add(line);
   }
   this.drawCells = function() {
     this.clearBoard()
@@ -146,23 +105,14 @@ function Board(width, height, pixelWidth, context) {
     this.context.clearRect(0, 0, this.width, this.height)
   }
 
-  this.canMoveLeft = function(x, y) {
-    if ((y > this.line.y && y < this.line.y + this.line.height + 5)
-           && (x < this.line.x + this.line.width + 1 && x > this.line.x)) {
-      return false;
+  this.isBlocked = function(x, y) {
+    for (var i = 0; i < this.lines.length; i++) {
+      if ((x >= this.lines[i].x && x <= (this.lines[i].x + this.lines[i].width)) &&
+        (y >= this.lines[i].y && y <= (this.lines[i].y + this.lines[i].height))) {
+        return true
+      }
     }
-    return (x !== 0);
-  }
-  this.canJumpTo = function(x, y) {
-    return (x < this.line.x || x > this.line.x + this.line.width)
-          && (y > this.line.y + this.line.height || y < this.line.y)
-  }
-  this.canMoveRight = function(x, y) {
-    if ((y > this.line.y && y < this.line.y + this.line.height)
-        && (x < this.line.x + this.line.width + 1 && x > this.line.x)) {
-      return false;
-    }
-    return (x !== this.width - 1);
+    return false;
   }
 
   this.turn = function() {
@@ -180,15 +130,6 @@ function Board(width, height, pixelWidth, context) {
     }
   }
 
-  this.onGround = function(thing) {
-    if (thing.y >= this.height - 1) {
-      this.y = this.height;
-      return true;
-    } else {
-      return false;
-    }
-  }
-
   this.keyUpDown = function(e){
     e = e || event; // to deal with IE
     board.keyMap[e.keyCode] = e.type == 'keydown';
@@ -203,17 +144,27 @@ var start = function() {
 
   board = new Board(200, 200, 1, context);
 
-  var line = new Line(board, board.width / 2 - 10, board.height - 20)
+  var line = new Line(board, board.width / 2 - 10, board.height - 20, 25, 5)
+  var line2 = new Line(board, board.width / 2 - 5, board.height - 40, 25, 5)
+  var line3 = new Line(board, board.width / 2, board.height - 60, 25, 5)
+  var bottom = new Line(board, 0, board.height - 1, board.width, 1)
+  var left = new Line(board, 0, 0, 1, board.height)
+  var right = new Line(board, board.width - 1, 0, 1, board.height)
 
-  var man = new Man(board, line, board.width / 2 - 10, board.height - 1)
+  var man = new Man(board, board.width / 2 - 10, board.height - 2 - 10, 10, 5)
 
 
 
   board.add(man);
-  board.add(line);
-
   board.addMan(man);
-  board.addLine(line);
+
+  board.addLines(line);
+  board.addLines(line2);
+  board.addLines(line3);
+  board.addLines(bottom);
+  board.addLines(left);
+  board.addLines(right);
+
 
   document.onkeydown = board.keyUpDown;
   document.onkeyup = board.keyUpDown;
