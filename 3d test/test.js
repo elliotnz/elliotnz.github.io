ae87f7f7dfc10357a25b200b5fef40fda73f56cc
Elliot <elliot.baguley@gmail.com>
2015-02-17 21:23:02 +1300

    Rotation

--- 3d test/test.js
+++ 3d test/test.js
@@ -1,38 +1,78 @@
 function Point(x, y) {
   this.x = x;
   this.y = y;
+
+  this.draw = function(ctx, style) {
+    ctx.beginPath();
+    ctx.arc(x,y,5,0,2*Math.PI);
+    ctx.stroke();
+  }
 }
 
-function Shape(lines) {
-  this.lines = lines;
+function Shape(segments, verticies) {
+  this.segments = segments;
+  this.verticies = verticies;
 
-  this.draw = function(ctx) {
-
-    for (var lIndex = 0; lIndex < this.lines.length; lIndex++) {
+  this.draw = function(ctx, style) {
+    for (var iSegment = 0; iSegment < this.segments.length; iSegment++) {
       ctx.beginPath();
-      for (var i = 0; i < this.lines[lIndex].length; i++) {
-        var line =  this.lines[lIndex];
-        for (var j = 0; j < line.length; j++) {
-          var point = line[j];
-          if (j === 0) {
-            ctx.moveTo(point.x, point.y);
-          } else {
-            ctx.lineTo(point.x, point.y);
-          }
+      ctx.strokeStyle = style.toString();
+      var lines = this.segments[iSegment];
+      for (var i = 0; i < lines.length; i++) {
+        var point = lines[i];
+        if (i === 0) {
+          ctx.moveTo(point.x, point.y);
+        } else {
+          ctx.lineTo(point.x, point.y);
         }
       }
       ctx.stroke();
     }
   }
 
-  this.rotate = function(degreesToRotate, point) {
 
+  // sin(o) = o/h
+  this.rotate = function(degreesToRotate, origin) {
+    var radians = degreesToRotate * (Math.PI / 180.0);
+    for (var i = 0; i < this.verticies.length; i++) {
+      var point = this.verticies[i];
+      // first with pythagorus to get length H
+      //  c**2 = a**2 + b**2
+      var a2 = Math.abs(point.x - origin.x) * Math.abs(point.x - origin.x);
+      var b2 = Math.abs(point.y - origin.y) * Math.abs(point.y - origin.y);
+      var hyp = Math.sqrt(a2 + b2); // c or hypotenuse
+      var opp = Math.sin(radians) * hyp;
+      var adj = opp / Math.tan(radians);
+      var o = Math.sin(radians) * adj;
+      var a = o / Math.tan(radians);
+      // new x and y
+      var newX = point.x + a;
+      var newY = point.y + o;
+      console.log("Old point (" + point.x + ", " + point.y + ")")
+      point.x = Math.floor(newX);
+      point.y = Math.floor(newY);
+      console.log("New point (" + point.x + ", " + point.y + ")")
+    }
+    this.debug()
+  }
+
+  this.debug = function() {
+    for (var i = 0; i < this.verticies.length; i++) {
+      point = this.verticies[i]
+      console.log("point (" + point.x + ", " + point.y + ")")
+    }
 
   }
 }
 
 function Square(x, y, w, h) {
-  this.lines = [[new Point(x ,y), new Point(x + w, y), new Point(x + w, y + h), new Point(x, y + h), new Point(x, y)]]
+  this.verticies = [new Point(x ,y), new Point(x + w, y), new Point(x + w, y + h), new Point(x, y + h)]
+  this.segments =
+    [
+      [this.verticies[0], this.verticies[1], this.verticies[2], this.verticies[3], this.verticies[0]],
+      [this.verticies[0], this.verticies[2]]
+    ];
+
 }
 Square.prototype = new Shape();
 
@@ -48,9 +88,9 @@ function Board(width, height, pixelWidth, context) {
   this.context.canvas.style.height = '' + (height * pixelWidth) + 'px';
   this.shapes = new Array()
 
-  this.draw = function() {
+  this.draw = function(style) {
     for (var i in this.shapes) {
-      this.shapes[i].draw(this.context);
+      this.shapes[i].draw(this.context, style);
     }
   }
 
@@ -60,17 +100,20 @@ var getBoard = function() {
   var canvas = document.getElementById("myCanvas");
   var context = canvas.getContext("2d");
 
-  return(new Board(500, 250, 1, context));
+  return(new Board(1000, 500, 1, context));
 }
 
 var start = function() {
   var board = getBoard();
-  var mySquare = new Square(100, 100, 50, 30);
+  var rotateOrigin = new Point(50, 50);
+  var mySquare = new Square(100, 100, 100, 100);
+
   board.shapes.push(mySquare);
+  board.shapes.push(rotateOrigin);
 
-  board.draw()
+  board.draw("black")
 
-  mySquare.rotate(20, new Point(100, 100))
+  mySquare.rotate(25, rotateOrigin)
 
-  board.draw();
+  board.draw("red");
 }
