9ebd6779cd67df360020ef15ab278b30bb1bfbc9
Marcus Baguley <marcus.baguley@gmail.com>
2016-05-24 21:35:05 +1200

    Added more balls

--- ball-arena/arena.js
+++ ball-arena/arena.js
@@ -4,65 +4,77 @@ function Game() {
   this.keyMap = [];
   this.alert = false;
   this.balls = [];
-  this.ball = null;
-  this.ballPosition = null;
   this.browserWidth = null;
   this.browserHeight = null;
+  this.playerBalls = [];
 
   this.addball = function(ball) {
     var node = document.createElement("div");
     node.setAttribute("class", "ball");
     node.style.position = "absolute";
     node.style.borderRadius = "50%";
+
     if (!ball.player) {
       node.style.zIndex="-1"
     }
     node.style.marginLeft = (((window.innerWidth || document.body.clientWidth) - ball.size) / 2) + "px";
-    node.style.marginTop = (((window.innerHeight || document.body.clientHeight) - ball.size) / 2) + "px";
+    node.style.marginTop = (((window.innerHeight || document.body.clientHeight) - ball.size) / 4 + (this.balls.length - 1) * 100) + "px";
     node.style.width = ball.size + "px";
     node.style.height = ball.size + "px";
-    node.style.backgroundColor = ball.colour;
 
     document.getElementsByTagName("body")[0].appendChild(node);
     this.balls.push(ball);
   }
 
-  this.getball = function() {
+  this.getballs = function() {
     for (var i in this.balls) {
       if (this.balls[i].player) {
-        this.ball = this.balls[i];
-        this.ballPosition = i;
-        break;
+        this.playerBalls.push(this.balls[i]);
+        this.balls[i].myPosition = i;
+        //this.ballPosition = i;
+        //break;
       }
     }
   }
 
   this.turn = function() {
-    if (this.ball == null) {
-      this.getball();
+    if (this.playerBalls.length === 0) {
+      this.getballs();
     }
-    this.browserWidth = (window.innerWidth || document.body.clientWidth) - parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.width);
-    this.browserHeight = (window.innerHeight || document.body.clientHeight) - parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.height);
+    this.browserWidth = (window.innerWidth || document.body.clientWidth);
+    this.browserHeight = (window.innerHeight || document.body.clientHeight);
     for (var i in this.balls) {
       this.balls[i].turn();
     }
     if (this.keyMap['37']) {
-      this.ball.moveHorizontal(-1);
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].moveHorizontal(-1);
+      }
     }
     if (this.keyMap['39']) {
-      this.ball.moveHorizontal(1);
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].moveHorizontal(1);
+      }
     }
     if (!this.keyMap['37'] && !this.keyMap['39']) {
-      this.ball.notPressingHorizontal();
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].notPressingHorizontal();
+      }
     }
     if (this.keyMap['38']) {
-      this.ball.moveVertical(-1);
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].moveVertical(-1);
+      }
     }
     if (this.keyMap['40']) {
-      this.ball.moveVertical(1);
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].moveVertical(1);
+      }
     }
     if (!this.keyMap['38'] && !this.keyMap['40']) {
-      this.ball.notPressingVertical();
+      for (var i in this.playerBalls) {
+        this.playerBalls[i].notPressingVertical();
+      }
     }
   }
 
@@ -72,9 +84,9 @@ function Game() {
   }
 }
 
-function Ball(colour, player, size) {
-  this.colour = colour;
+function Ball(player, size) {
   this.player = player;
+  this.myPosition = null;
   this.size = size;
   this.x = null;
   this.y = null;
@@ -82,19 +94,19 @@ function Ball(colour, player, size) {
   this.speedY = 0;
   this.speedY = 0;
   this.drag = 1;
-  this.counter = 0;
-  this.currentColor = 1;
 
   this.turn = function() {
-    this.x = parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.marginLeft);
-    this.y = parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.marginTop);
+    if (this.player) {
+      this.x = parseInt(document.getElementsByClassName("ball")[this.myPosition].style.marginLeft);
+      this.y = parseInt(document.getElementsByClassName("ball")[this.myPosition].style.marginTop);
+    }
   }
 
   this.moveHorizontal = function(direction) {
-    var speed = direction * (10000 / (parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.width) * parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.height)));
+    var speed = direction * (10000 / (parseInt(document.getElementsByClassName("ball")[this.myPosition].style.width) * parseInt(document.getElementsByClassName("ball")[this.myPosition].style.height)));
     this.speedX += speed;
     speed = this.speedX;
-    var browserWidth = game.browserWidth;
+    var browserWidth = game.browserWidth - this.size;
     if ((this.x + speed) > 0 && (this.x + speed) < browserWidth) {
       this.x += speed;
     } else if ((this.x + speed) > 0 && speed < 0) {
@@ -106,14 +118,14 @@ function Ball(colour, player, size) {
     } else {
       this.speedX *= -0.5;
     }
-    document.getElementsByClassName("ball")[game.ballPosition].style.marginLeft = (this.x) + "px";
+    document.getElementsByClassName("ball")[this.myPosition].style.marginLeft = (this.x) + "px";
   }
 
   this.moveVertical = function(direction) {
-    var speed = direction * (10000 / (parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.width) * parseInt(document.getElementsByClassName("ball")[game.ballPosition].style.height)));
+    var speed = direction * (10000 / (parseInt(document.getElementsByClassName("ball")[this.myPosition].style.width) * parseInt(document.getElementsByClassName("ball")[this.myPosition].style.height)));
     this.speedY += speed;
     speed = this.speedY;
-    var browserHeight = game.browserHeight;
+    var browserHeight = game.browserHeight - this.size;
     if ((this.y + speed) > 0 && (this.y + speed) < browserHeight) {
       this.y += speed;
     } else if ((this.y + speed) > 0 && speed < 0) {
@@ -125,7 +137,7 @@ function Ball(colour, player, size) {
     } else {
       this.speedY *= -0.5;
     }
-    document.getElementsByClassName("ball")[game.ballPosition].style.marginTop = (this.y) + "px";
+    document.getElementsByClassName("ball")[this.myPosition].style.marginTop = (this.y) + "px";
   }
 
   this.notPressingHorizontal = function() {
@@ -138,7 +150,7 @@ function Ball(colour, player, size) {
         } else {
           this.speedX -= this.drag;
         }
-        var browserWidth = game.browserWidth;
+        var browserWidth = game.browserWidth - this.size;
         if ((this.x + this.speedX) > 0 && (this.x + this.speedX) < browserWidth) {
           this.x += this.speedX;
         } else if ((this.x + this.speedX) > 0 && this.speedX < 0) {
@@ -150,7 +162,7 @@ function Ball(colour, player, size) {
         } else {
           this.speedX *= -0.5;
         }
-        document.getElementsByClassName("ball")[game.ballPosition].style.marginLeft = (this.x) + "px";
+        document.getElementsByClassName("ball")[this.myPosition].style.marginLeft = (this.x) + "px";
       }
     }
   }
@@ -165,7 +177,7 @@ function Ball(colour, player, size) {
         } else {
           this.speedY -= this.drag;
         }
-        var browserHeight = game.browserHeight;
+        var browserHeight = game.browserHeight - this.size;
         if ((this.y + this.speedY) > 0 && (this.y + this.speedY) < browserHeight) {
           this.y += this.speedY;
         } else if ((this.y + this.speedY) > 0 && this.speedY < 0) {
@@ -177,15 +189,19 @@ function Ball(colour, player, size) {
         } else {
           this.speedY *= -0.5;
         }
-        document.getElementsByClassName("ball")[game.ballPosition].style.marginTop = (this.y) + "px";
+        document.getElementsByClassName("ball")[this.myPosition].style.marginTop = (this.y) + "px";
       }
     }
   }
 }
 
 function start() {
-  var ball3 = new Ball("red", true, 75);
-  game.addball(ball3);
+  game.addball(new Ball(true, 150));
+  //game.addball(new Ball(false, 75));
+  game.addball(new Ball(true, 50));
+  game.addball(new Ball(true, 100));
+  //game.addball(new Ball(false, 75));
+  game.addball(new Ball(true, 75));
 
   document.onkeydown = game.keyUpDown;
   document.onkeyup = game.keyUpDown;
